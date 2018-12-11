c9846fafd080aac8d0e51adc3668427f492c922a
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-12-11 22:10:55 +0900

    Refactor handle types from dgraph to handle_types.hpp to prevent circular includes. Add handle utility functions

--- src/dgraph.hpp
+++ src/dgraph.hpp
@@ -13,6 +13,8 @@
 #include <utility>
 #include <functional>
 #include "path.hpp"
+#include "handle_types.hpp"
+#include "handle_helper.hpp"
 #include "sdsl/bit_vectors.hpp"
 //#include "sdsl/enc_vector.hpp"
 //#include "sdsl/dac_vector.hpp"
@@ -23,20 +25,6 @@
 
 namespace dankgraph {
 
-/// represents an id
-typedef int64_t id_t;
-/// represents an offset
-typedef size_t off_t;
-/// represents a position
-typedef std::tuple<id_t, bool, off_t> pos_t;
-/// represents the internal id of a node traversal
-struct handle_t { char data[sizeof(id_t)]; };
-/// represents an edge in terms of its endpoints
-typedef std::pair<handle_t, handle_t> edge_t;
-/// represents the internal id of a path entity
-struct path_handle_t { char data[sizeof(int64_t)]; };
-/// An occurrence handle is an opaque reference to an occurrence of an oriented node along a path in a graph
-struct occurrence_handle_t { char data[2 * sizeof(int64_t)]; };
 
 class SuccinctDynamicSequenceGraph {
         
---
+++ src/handle_types.hpp
@@ -0,0 +1,23 @@
+#ifndef dank_graph_handle_types
+#define dank_graph_handle_types
+#include <cstdint>
+#include <utility>
+
+namespace dankgraph{
+/// represents an id
+typedef int64_t id_t;
+/// represents an offset
+typedef size_t off_t;
+/// represents a position
+typedef std::tuple<id_t, bool, off_t> pos_t;
+/// represents the internal id of a node traversal
+struct handle_t { char data[sizeof(id_t)]; };
+/// represents an edge in terms of its endpoints
+typedef std::pair<handle_t, handle_t> edge_t;
+/// represents the internal id of a path entity
+struct path_handle_t { char data[sizeof(int64_t)]; };
+/// An occurrence handle is an opaque reference to an occurrence of an oriented node along a path in a graph
+struct occurrence_handle_t { char data[2 * sizeof(int64_t)]; };
+}
+
+#endif
