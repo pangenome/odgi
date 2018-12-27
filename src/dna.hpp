0d7f1a67a702c77a4a18cc97678d084732a7ff81
Erik Garrison <erik.garrison@gmail.com>
2018-12-27 15:34:48 +0100

    DNA manipulation functions

---
+++ src/dna.hpp
@@ -0,0 +1,69 @@
+#ifndef dank_dna_hpp
+#define dank_dna_hpp
+
+#include <string>
+
+namespace dankgraph {
+
+static const char complement[256] = {'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 8
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 16
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 24
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 32
+                                     'N', 'N', 'N', '$', '#', 'N', 'N', 'N', // 40 GCSA stop/start characters
+                                     'N', 'N', 'N', 'N', 'N', '-', 'N', 'N', // 48
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 56
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 64
+                                     'N', 'T', 'V', 'G', 'H', 'N', 'N', 'C', // 72
+                                     'D', 'N', 'N', 'M', 'N', 'K', 'N', 'N', // 80
+                                     'N', 'Q', 'Y', 'W', 'A', 'A', 'B', 'S', // 88
+                                     'N', 'R', 'N', 'N', 'N', 'N', 'N', 'N', // 96
+                                     'N', 't', 'v', 'g', 'h', 'N', 'N', 'c', // 104
+                                     'd', 'N', 'N', 'm', 'N', 'k', 'n', 'N', // 112
+                                     'N', 'q', 'y', 'w', 'a', 'a', 'b', 's', // 120
+                                     'N', 'r', 'N', 'N', 'N', 'N', 'N', 'N', // 128
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 136
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 144
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 152
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 160
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 168
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 176
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 184
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 192
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 200
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 208
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 216
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 224
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 232
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 240
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', // 248
+                                     'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N'};// 256
+
+inline char reverse_complement(const char& c) {
+    return complement[c];
+}
+
+inline std::string reverse_complement(const std::string& seq) {
+    std::string rc;
+    rc.assign(seq.rbegin(), seq.rend());
+    for (auto& c : rc) {
+        c = complement[c];
+    }
+    return rc;
+}
+    
+inline void reverse_complement_in_place(std::string& seq) {
+    size_t swap_size = seq.size() / 2;
+    for (size_t i = 0, j = seq.size() - 1; i < swap_size; i++, j--) {
+        char tmp = seq[i];
+        seq[i] = complement[seq[j]];
+        seq[j] = complement[tmp];
+    }
+    
+    if (seq.size() % 2) {
+        seq[swap_size] = complement[seq[swap_size]];
+    }
+}
+
+}
+
+#endif
