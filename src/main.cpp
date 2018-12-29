76e6a274f09bc929a2eb2ffae3e5730396658ffb
Erik Garrison <erik.garrison@gmail.com>
2018-12-29 09:46:22 +0100

    start parameterizing main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "graph.hpp"
 #include "gfakluge.hpp"
+#include "args.hxx"
 //#include "io_helper.hpp"
 
 using namespace dankgraph;
@@ -17,9 +18,47 @@ void make_graph(void) {
 }
 
 int main(int argc, char** argv) {
+
+    args::ArgumentParser parser("dankgraph: succinct, dynamic variation graph");
+    args::HelpFlag help(parser, "help", "display this help summary", {'h', "help"});
+    args::ValueFlag<std::string> gfa_file(parser, "FILE", "construct the graph from this GFA input file", {'g', "gfa"});
+    args::ValueFlag<std::string> dg_out_file(parser, "FILE", "store the index in this file", {'o', "out"});
+    args::ValueFlag<std::string> dg_in_file(parser, "FILE", "load the index from this file", {'i', "idx"});
+    //args::ValueFlag<std::string> seqs(parser, "FILE", "the sequences used to generate the alignments", {'s', "seqs"});
+    //args::ValueFlag<std::string> base(parser, "FILE", "build graph using this basename", {'b', "base"});
+    //args::ValueFlag<uint64_t> num_threads(parser, "N", "use this many threads during parallel steps", {'t', "threads"});
+    //args::ValueFlag<uint64_t> repeat_max(parser, "N", "limit transitive closure to include no more than N copies of a given input base", {'r', "repeat-max"});
+    //args::ValueFlag<uint64_t> aln_keep_n_longest(parser, "N", "keep up to the N-longest alignments overlapping each query position", {'k', "aln-keep-n-longest"});
+    //args::ValueFlag<uint64_t> aln_min_length(parser, "N", "ignore alignments shorter than this", {'m', "aln-min-length"});
+    args::Flag debug(parser, "debug", "enable debugging", {'d', "debug"});
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help) {
+        std::cout << parser;
+        return 0;
+    } catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+    if (argc==1) {
+        std::cout << parser;
+        return 1;
+    }
+
+    /*
+    size_t n_threads = args::get(num_threads);
+    if (n_threads) {
+        omp_set_num_threads(args::get(num_threads));
+    } else {
+        omp_set_num_threads(1);
+    }
+    */
+    
     //make_graph();
     assert(argc > 0);
-    char* filename = argv[1];
+    std::string gfa_filename = args::get(gfa_file);
+    char* filename = (char*)gfa_filename.c_str();
     //std::cerr << "filename is " << filename << std::endl;
     gfak::GFAKluge gg;
     //double version = gg.detect_version_from_file(filename);
@@ -46,6 +85,8 @@ int main(int argc, char** argv) {
             }
         });
     // here we should measure memory usage etc.
-    graph.display();
+    if (args::get(debug)) {
+        graph.display();
+    }
     return 0;
 }
