3f9275a16d107eb6d9062cf57eec34b3dd8ea76b
Erik Garrison <erik.garrison@gmail.com>
2019-02-01 11:42:37 +0000

    exploit performance recovered by reverting to std::vector<uint64_t> for subtree and psum sizes

--- src/dynamic_types.hpp
+++ src/dynamic_types.hpp
@@ -5,10 +5,10 @@
 
 namespace dankgraph {
 
-typedef dyn::succinct_bitvector<dyn::spsi<dyn::packed_vector,2048,2> > suc_bv;
+typedef dyn::succinct_bitvector<dyn::spsi<dyn::packed_vector,256,16> > suc_bv;
 //typedef dyn::spsi<dyn::packed_vector,1024,1> spsi_iv;
-typedef dyn::lciv<dyn::packed_vector,1024,1> lciv_iv;
-typedef dyn::wt_string<dyn::succinct_bitvector<dyn::spsi<dyn::packed_vector,2048,2> > > wt_str;
+typedef dyn::lciv<dyn::packed_vector,256,16> lciv_iv;
+typedef dyn::wt_string<dyn::succinct_bitvector<dyn::spsi<dyn::packed_vector,256,16> > > wt_str;
 
 }
 
