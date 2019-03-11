52f43dfa4ad2e6ad8de4f08430c2ca796dbaccd1
Erik Garrison <erik.garrison@gmail.com>
2019-03-11 16:24:49 +0000

    make the fuzzy graph test big

--- src/unittest/fuzz.cpp
+++ src/unittest/fuzz.cpp
@@ -19,7 +19,7 @@ namespace unittest {
 using namespace std;
 using namespace handlegraph;
 
-TEST_CASE("Handle graphs can handle huge numbers of nodes", "[handle][packed][hashgraph]") {
+TEST_CASE("Large random handle with high path depth work", "[handle][packed][hashgraph]") {
     
     vector<MutablePathDeletableHandleGraph*> implementations;
     
@@ -31,18 +31,22 @@ TEST_CASE("Handle graphs can handle huge numbers of nodes", "[handle][packed][ha
         MutablePathDeletableHandleGraph& graph = *implementation;
         std::vector<handle_t> handles;
         std::mt19937 rng(87);
-        for (int i = 0; i < 1000; ++i) {
-            handles.push_back(graph.create_handle("A"));
+        int num_handles = 1e6;
+        std::uniform_int_distribution<std::mt19937::result_type> dist(1,num_handles);
+        std::uniform_int_distribution<std::mt19937::result_type> lte5(1,5);
+        std::uniform_int_distribution<std::mt19937::result_type> lte1000(1,1000);
+        for (int i = 0; i < num_handles; ++i) {
+            std::string s('A', lte1000(rng));
+            handles.push_back(graph.create_handle(s));
+            REQUIRE(graph.get_sequence(handles.back()) == s);
         }
-        std::uniform_int_distribution<std::mt19937::result_type> dist(1,1000); // distribution in range [1, 1000]
-        std::uniform_int_distribution<std::mt19937::result_type> lte10(1,10);
         for (auto& handle_a : handles) {
-            for (int i = 0; i < lte10(rng); ++i) {
+            for (int i = 0; i < lte5(rng); ++i) {
                 handle_t handle_b = graph.get_handle(dist(rng));
                 graph.create_edge(handle_a, handle_b);
                 REQUIRE(graph.has_edge(handle_a, handle_b));
             }
-            for (int i = 0; i < lte10(rng); ++i) {
+            for (int i = 0; i < lte5(rng); ++i) {
                 handle_t handle_b = graph.get_handle(dist(rng));
                 graph.create_edge(graph.flip(handle_a), handle_b);
                 REQUIRE(graph.has_edge(graph.flip(handle_a), handle_b));
@@ -50,11 +54,9 @@ TEST_CASE("Handle graphs can handle huge numbers of nodes", "[handle][packed][ha
         }
 
         for (int p = 0; p < 100; ++p) {
-            //std::cerr << "path " << p << std::endl;
             path_handle_t path = graph.create_path_handle(std::to_string(p));
             handle_t last; // ...
-            for (int i = 0; i < 1000; ++i) {
-                //std::cerr << "occ " << i << std::endl;
+            for (int i = 0; i < 100000; ++i) {
                 handle_t occ = handles.at(dist(rng)-1);
                 graph.append_occurrence(path, occ);
                 if (i > 0) {
