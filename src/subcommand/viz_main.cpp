99347da745043287801a49f684741a65efa8d4e1
Erik Garrison <erik.garrison@gmail.com>
2019-03-14 19:06:39 +0000

    square edge plots!

--- src/subcommand/viz_main.cpp
+++ src/subcommand/viz_main.cpp
@@ -134,7 +134,7 @@ int main_viz(int argc, char** argv) {
 
     float alpha_value = 255*(args::get(alpha) ? args::get(alpha) : 1);
 
-    auto add_contact = [&](const uint64_t& _x, const uint64_t& _y) {
+    auto add_point = [&](const uint64_t& _x, const uint64_t& _y) {
         uint64_t x = std::min((uint64_t)std::round(_x * scale), width-1);
         uint64_t y = std::min((uint64_t)std::round(_y * scale), height-1);
         uint8_t v = alpha_value;
@@ -148,31 +148,88 @@ int main_viz(int argc, char** argv) {
         *a = 255;
     };
 
+    auto add_edge = [&](const handle_t& h, const handle_t& o) {
+        auto& _a = position_map[number_bool_packing::unpack_number(h)];
+        auto& _b = position_map[number_bool_packing::unpack_number(o)];
+        uint64_t a = std::min(_a, _b);
+        uint64_t b = std::max(_a, _b);
+        uint64_t dist = b - a;
+        uint64_t i = 0;
+        for ( ; i < dist; i+=1/scale) {
+            add_point(a, i);
+        }
+        while (a < b) {
+            add_point(a, i);
+            a += 1/scale;
+        }
+        for (uint64_t j = 0; j < dist; j+=1/scale) {
+            add_point(b, j);
+        }
+    };
+
     graph.for_each_handle([&](const handle_t& h) {
             uint64_t p = position_map[number_bool_packing::unpack_number(h)];
             uint64_t hl = graph.get_length(h);
             // make contects for the bases in the node
-            for (uint64_t i = 0; i < hl-1; ++i) {
-                add_contact(p+i, p+i+1);
+            for (uint64_t i = 0; i < hl; ++i) {
+                add_point(p+i, 0);
             }
         });
 
-
+    std::cerr << std::endl;
+    uint64_t seen = 0;
     graph.for_each_handle([&](const handle_t& h) {
+            if (++seen%100==0) std::cerr << "adding edges " << seen << "\r";
             // add contacts for the edges
-            uint64_t hl = graph.get_length(h);
             graph.follow_edges(h, false, [&](const handle_t& o) {
-                    uint64_t x = position_map[number_bool_packing::unpack_number(h)];
-                    uint64_t y = position_map[number_bool_packing::unpack_number(o)];
-                    add_contact(x, y);
+                    add_edge(h, o);
                 });
+            /*
             graph.follow_edges(h, true, [&](const handle_t& o) {
-                    uint64_t x = position_map[number_bool_packing::unpack_number(o)];
-                    uint64_t y = position_map[number_bool_packing::unpack_number(h)];
-                    add_contact(x, y);
+                    add_edge(o, h);
                 });
+            */
         });
 
+    /*
+    float path_alpha_value = 1-(args::get(path_alpha) ? args::get(path_alpha) : 1);
+    
+    graph.for_each_path_handle([&](const path_handle_t& path) {
+            uint32_t path_name_hash = djb2_hash32(graph.get_path_name(path).c_str());
+            uint8_t path_r = 0;
+            uint8_t path_b = 0;
+            uint8_t path_g = 0;
+            memcpy(&path_r, &path_name_hash, sizeof(uint8_t));
+            memcpy(&path_b, ((uint8_t*)&path_name_hash)+1, sizeof(uint8_t));
+            memcpy(&path_g, ((uint8_t*)&path_name_hash)+2, sizeof(uint8_t));
+            float path_r_f = (float)path_r/(float)255;
+            float path_g_f = (float)path_g/(float)255;
+            float path_b_f = (float)path_b/(float)255;
+            float sum = path_r_f + path_g_f + path_b_f;
+            path_r_f /= sum;
+            path_g_f /= sum;
+            path_b_f /= sum;
+            std::cerr << "path " << as_integer(path) << " " << graph.get_path_name(path) << " " << path_r_f << " " << path_g_f << " " << path_b_f << std::endl;
+            /// Loop over all the occurrences along a path, from first through last
+            //uint64_t x=0, y=0;
+            uint64_t step = 0;
+            graph.for_each_occurrence_in_path(path, [&](const occurrence_handle_t& occ) {
+                    //std::cerr << "occ " << ++step << "\r";
+                    handle_t h = graph.get_occurrence(occ);
+                    uint64_t p = position_map[number_bool_packing::unpack_number(h)];
+                    // todo...
+                    uint64_t hl = graph.get_length(h);
+                    // make contects for the bases in the node
+                    for (uint64_t i = 0; i < hl; ++i) {
+                        add_point(p+i, 0, path_r, path_g, path_b, alpha_value, as_integer(path)+1);
+                        //add_contact(p+i, p+i+1, 1, 0, 0, path_alpha_value);
+                    }
+                    //x = p+hl;
+                    //y = p+hl;
+                 });
+         });
+    */
+    
     png::encodeOneStep(filename, image, width, height);
 
     return 0;
