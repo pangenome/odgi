b03d04f8a99346909cde9fbf37b425225a7a66bc
Erik Garrison <erik.garrison@gmail.com>
2019-04-17 11:22:37 +0200

    pack paths in viz
    This can optionally be disabled, but it yields a much nicer visualization where
    the graph is partially ordered.

--- src/subcommand/viz_main.cpp
+++ src/subcommand/viz_main.cpp
@@ -3,7 +3,9 @@
 #include "args.hxx"
 #include "threads.hpp"
 #include "algorithms/hash.hpp"
+#include "algorithms/id_ordered_paths.hpp"
 #include "lodepng.h"
+#include <limits>
 #include <iostream>
 
 namespace odgi {
@@ -77,6 +79,9 @@ int main_viz(int argc, char** argv) {
     args::ValueFlag<uint64_t> image_width(parser, "N", "width in pixels of output image", {'x', "width"});
     args::ValueFlag<uint64_t> image_height(parser, "N", "height in pixels of output image", {'y', "height"});
     args::ValueFlag<uint64_t> path_height(parser, "N", "path display height", {'P', "path-height"});
+    args::ValueFlag<uint64_t> path_x_pad(parser, "N", "path x padding", {'X', "path-x-padding"});
+    args::Flag path_per_row(parser, "bool", "display a single path per row rather than packing them", {'R', "path-per-row"});
+    args::ValueFlag<float> link_path_pieces(parser, "FLOAT", "show thin links of this relative width to connect path pieces", {'L', "link-path-pieces"});
     args::ValueFlag<uint64_t> threads(parser, "N", "number of threads to use", {'t', "threads"});
 
     try {
@@ -129,10 +134,14 @@ int main_viz(int argc, char** argv) {
 
     uint64_t path_count = graph.get_path_count();
     uint64_t pix_per_path = (args::get(path_height) ? args::get(path_height) : 10);
+    uint64_t pix_per_link = std::max((uint64_t)1, (uint64_t)std::round(args::get(link_path_pieces)*pix_per_path));
+    uint64_t link_pix_y = pix_per_path/2 - pix_per_link/2;
     uint64_t path_space = path_count * pix_per_path;
+    uint64_t path_padding = args::get(path_x_pad);
+    uint64_t bottom_padding = 5;
     // the math here only works if the image size is greater than or equal to the graph length
     uint64_t width = std::min(len, (args::get(image_width) ? args::get(image_width) : 1000));
-    uint64_t height = std::min(len, (args::get(image_height) ? args::get(image_height) : 1000));
+    uint64_t height = std::min(len, (args::get(image_height) ? args::get(image_height) : 1000) + bottom_padding);
     std::vector<uint8_t> image;
     image.resize(width * (height + path_space) * 4, 255);
     float scale_x = (float)width/(float)len;
@@ -199,7 +208,57 @@ int main_viz(int argc, char** argv) {
             image[4 * width * y + 4 * x + 3] = 255;
         }
     };
+
+    auto add_path_link = [&](const uint64_t& _x, const uint64_t& _y,
+                             const uint8_t& _r, const uint8_t& _g, const uint8_t& _b) {
+        uint64_t x = std::min((uint64_t)std::round(_x * scale_x), width-1);
+        uint64_t t = _y*pix_per_path + link_pix_y;
+        uint64_t s = t+pix_per_link;
+        for (uint64_t y = t; y < s; ++y) {
+            image[4 * width * y + 4 * x + 0] = _r;
+            image[4 * width * y + 4 * x + 1] = _g;
+            image[4 * width * y + 4 * x + 2] = _b;
+            image[4 * width * y + 4 * x + 3] = 255;
+        }
+    };
     
+    // map from path id to its starting y position
+    //hash_map<uint64_t, uint64_t> path_layout_y;
+    std::vector<uint64_t> path_layout_y; path_layout_y.resize(path_count);
+    if (args::get(path_per_row)) {
+        for (uint64_t i = 0; i < path_count; ++i) {
+            path_layout_y[i] = i;
+        }
+    } else { // pack the paths
+        // buffer to record layout bounds
+        std::vector<bool> path_layout_buf; path_layout_buf.resize(path_count * width);
+        std::vector<path_handle_t> path_order = algorithms::id_ordered_paths(graph);
+        for (auto& path : path_order) {
+            // get the block which this path covers
+            uint64_t min_x = std::numeric_limits<uint64_t>::max();
+            uint64_t max_x = std::numeric_limits<uint64_t>::min(); // 0
+            graph.for_each_occurrence_in_path(path, [&](const occurrence_handle_t& occ) {
+                    handle_t h = graph.get_occurrence(occ);
+                    uint64_t p = position_map[number_bool_packing::unpack_number(h)];
+                    min_x = std::min(min_x, p);
+                    max_x = std::max(max_x, p+graph.get_length(h));
+                });
+            //std::cerr << "min and max x " << min_x << " " << max_x << " vs " << len << std::endl;
+            // now find where this would fit and mark the layout buffer
+            // due to our sorted paths, we are able to drop to the lowest available layout position
+            uint64_t path_y = 0;
+            uint64_t actual_x = min_x*scale_x;
+            while (path_layout_buf[width * path_y + actual_x] && path_y+1 < path_count) {
+                ++path_y;
+            }
+            for (uint64_t i = min_x*scale_x; i < std::min((uint64_t)(max_x*scale_x + path_padding), width); ++i) {
+                path_layout_buf[width * path_y + i] = 1;
+            }
+            //std::cerr << "path_y " << graph.get_path_name(path) << " " << path_count - path_y - 1 << std::endl;
+            path_layout_y[as_integer(path)] = path_count - path_y - 1;
+        }
+    }
+
     graph.for_each_path_handle([&](const path_handle_t& path) {
             uint32_t path_name_hash = djb2_hash32(graph.get_path_name(path).c_str());
             uint8_t path_r = 0;
@@ -226,16 +285,63 @@ int main_viz(int argc, char** argv) {
             graph.for_each_occurrence_in_path(path, [&](const occurrence_handle_t& occ) {
                     handle_t h = graph.get_occurrence(occ);
                     uint64_t p = position_map[number_bool_packing::unpack_number(h)];
-                    // todo...
                     uint64_t hl = graph.get_length(h);
                     // make contects for the bases in the node
+                    uint64_t path_y = path_layout_y[path_rank];
                     for (uint64_t i = 0; i < hl; i+=1/scale_x) {
-                        add_path_step(p+i, path_rank, path_r, path_g, path_b);
+                        add_path_step(p+i, path_y, path_r, path_g, path_b);
                     }
                  });
+            // add in a visual motif that shows the links between path pieces
+            // this is most meaningful in a linear layout
+            if (args::get(link_path_pieces)) {
+                uint64_t min_x = std::numeric_limits<uint64_t>::max();
+                uint64_t max_x = std::numeric_limits<uint64_t>::min(); // 0
+                graph.for_each_occurrence_in_path(path, [&](const occurrence_handle_t& occ) {
+                        handle_t h = graph.get_occurrence(occ);
+                        uint64_t p = position_map[number_bool_packing::unpack_number(h)];
+                        min_x = std::min(min_x, p);
+                        max_x = std::max(max_x, p+graph.get_length(h));
+                    });
+                // now touch up the range
+                uint64_t path_y = path_layout_y[path_rank];
+                for (uint64_t i = min_x; i < max_x; i+=1/scale_x) {
+                    add_path_link(i, path_y, path_r, path_g, path_b);
+                }
+            }
          });
+
+    // trim vertical space to fit
+    uint64_t min_y = std::numeric_limits<uint64_t>::max();
+    uint64_t max_y = std::numeric_limits<uint64_t>::min(); // 0
+    for (uint64_t y = 0; y < height + path_space; ++y) {
+        for (uint64_t x = 0; x < width; ++x) {
+            uint8_t r = image[4 * width * y + 4 * x + 0];
+            uint8_t g = image[4 * width * y + 4 * x + 1];
+            uint8_t b = image[4 * width * y + 4 * x + 2];
+            uint8_t a = image[4 * width * y + 4 * x + 3];
+            if (r != 255 || g != 255 || b != 255) {
+                min_y = std::min(min_y, y);
+                max_y = std::max(max_y, y);
+            }
+        }
+    }
+    // provide some default padding at the bottom, to clarify the edges
+    max_y = std::min(path_space + height, max_y + bottom_padding);
+    //std::cerr << "min and max y " << min_y << " " << max_y << std::endl;
+    std::vector<uint8_t> crop;
+    uint64_t crop_height = max_y - min_y;
+    crop.resize(width * crop_height * 4, 255);
+    for (uint64_t y = 0; y < max_y - min_y; ++y) {
+        for (uint64_t x = 0; x < width; ++x) {
+            crop[4 * width * y + 4 * x + 0] = image[4 * width * (y + min_y) + 4 * x + 0];
+            crop[4 * width * y + 4 * x + 1] = image[4 * width * (y + min_y) + 4 * x + 1];
+            crop[4 * width * y + 4 * x + 2] = image[4 * width * (y + min_y) + 4 * x + 2];
+            crop[4 * width * y + 4 * x + 3] = image[4 * width * (y + min_y) + 4 * x + 3];
+        }
+    }
     
-    png::encodeOneStep(filename, image, width, height + path_space);
+    png::encodeOneStep(filename, crop, width, crop_height);
 
     return 0;
 }
