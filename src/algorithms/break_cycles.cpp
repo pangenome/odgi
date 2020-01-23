b86825800ecf68dbfb0c05c1c42da922a54d06f7
Erik Garrison <erik.garrison@gmail.com>
2020-01-23 11:17:40 +0100

    explore for small cycles in both orientations

--- src/algorithms/break_cycles.cpp
+++ src/algorithms/break_cycles.cpp
@@ -28,50 +28,55 @@ std::vector<edge_t> edges_inducing_cycles(
     ska::flat_hash_set<edge_t> edges_to_remove;
 
     graph.for_each_handle(
-        [&](const handle_t& root_handle) {
+        [&](const handle_t& handle) {
             //std::cerr << "on handle " << graph.get_id(root_handle) << std::endl;
             uint64_t seen_bp = 0;
             uint64_t max_depth = 0;
             uint64_t last_min_length_bp = 0; //std::numeric_limits<uint64_t>::max();
             uint64_t curr_min_length_bp = std::numeric_limits<uint64_t>::max();
-            bfs(graph,
-                [&graph,&edges_to_remove,&seen_bp,&curr_min_length_bp,&last_min_length_bp,&max_depth]
-                (const handle_t& h, const uint64_t& r, const uint64_t& l, const uint64_t& d) {
-                    /*
-                    std::cerr << "stepping onto " << graph.get_id(h) << (graph.get_is_reverse(h)?"-":"+")
-                              << " (" << r << " " << l << " " << d << ")"
-                              << std::endl;
-                    */
-                    // track the minimum length at the last step
-                    if (d > max_depth) {
-                        max_depth = d;
-                        last_min_length_bp = curr_min_length_bp;
-                        curr_min_length_bp = l;
-                    } else {
-                        curr_min_length_bp = std::min(l, curr_min_length_bp);
-                    }
-                    seen_bp += graph.get_length(h);
-                },
-                [](const handle_t& h) { return false; },
-                [&root_handle,&edges_to_remove](const handle_t& p, const handle_t& h) {
-                    edge_t e = std::make_pair(p, h);
-                    if (h == root_handle) {
-                        edges_to_remove.insert(e);
-                        return true;
-                    } else if (edges_to_remove.count(e)) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-                [&last_min_length_bp,&max_cycle_size,&seen_bp,&max_search_bp](void) {
-                    //std::cerr << "min_length_bp " << last_min_length_bp << " seen_bp " << seen_bp << std::endl;
-                    return last_min_length_bp > max_cycle_size
-                        || seen_bp > max_search_bp;
-                },
-                { root_handle },
-                { },
-                false); // don't use bidirectional search
+            handle_t handle_fwd = handle;
+            handle_t handle_rev = graph.flip(handle);
+            for (const handle_t& root_handle : { handle_fwd, handle_rev }) {
+                bfs(graph,
+                    [&graph,&edges_to_remove,&seen_bp,&curr_min_length_bp,&last_min_length_bp,&max_depth]
+                    (const handle_t& h, const uint64_t& r, const uint64_t& l, const uint64_t& d) {
+                        /*
+                          std::cerr << "stepping onto " << graph.get_id(h) << (graph.get_is_reverse(h)?"-":"+")
+                          << " (" << r << " " << l << " " << d << ")"
+                          << std::endl;
+                        */
+                        // track the minimum length at the last step
+                        if (d > max_depth) {
+                            max_depth = d;
+                            last_min_length_bp = curr_min_length_bp;
+                            curr_min_length_bp = l;
+                        } else {
+                            curr_min_length_bp = std::min(l, curr_min_length_bp);
+                        }
+                        seen_bp += graph.get_length(h);
+                    },
+                    [](const handle_t& h) { return false; },
+                    [&graph,&root_handle,&edges_to_remove](const handle_t& p, const handle_t& h) {
+                        edge_t e = std::make_pair(p, h);
+                        if (h == root_handle) {
+                            edges_to_remove.insert(e);
+                            return true;
+                        } else if (edges_to_remove.count(e)
+                                   || edges_to_remove.count(std::make_pair(graph.flip(h), graph.flip(p)))) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    },
+                    [&last_min_length_bp,&max_cycle_size,&seen_bp,&max_search_bp](void) {
+                        //std::cerr << "min_length_bp " << last_min_length_bp << " seen_bp " << seen_bp << std::endl;
+                        return last_min_length_bp > max_cycle_size
+                            || seen_bp > max_search_bp;
+                    },
+                    { root_handle },
+                    { },
+                    false); // don't use bidirectional search
+            }
         });
 
     std::vector<edge_t> edges;
