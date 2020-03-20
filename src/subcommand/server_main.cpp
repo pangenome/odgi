e942096eef8df8abc5a0290e1dac7c85f4257ed6
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2020-03-20 18:19:50 +0100

    cors

--- src/subcommand/server_main.cpp
+++ src/subcommand/server_main.cpp
@@ -102,6 +102,7 @@ namespace odgi {
             }
             std::cout << "SEND RESPONSE: pangenome position: " << pan_pos << std::endl;
             res.set_header("Access-Control-Allow-Origin", "*");
+            res.set_header("Access-Control-Expose-Headers", "text/plain");
             res.set_content(std::to_string(pan_pos), "text/plain");
         });
 
