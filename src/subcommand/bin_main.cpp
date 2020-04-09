bc33d9e435ced00ff231194928087e04738a9c08
Artem Tarasov <lomereiter@gmail.com>
2020-04-09 21:57:28 +0200

    parquet output

--- src/subcommand/bin_main.cpp
+++ src/subcommand/bin_main.cpp
@@ -3,6 +3,11 @@
 #include "args.hxx"
 #include "algorithms/bin_path_info.hpp"
 
+#include <arrow/io/file.h>
+#include <parquet/exception.h>
+#include <parquet/stream_writer.h>
+
+#include <filesystem>
 #include <regex>
 
 namespace odgi {
@@ -109,6 +114,181 @@ struct JsonSerializer : public algorithms::BinSerializer {
     }
 };
 
+parquet::schema::NodePtr UInt64(const std::string& name) {
+    using namespace parquet;
+    return schema::PrimitiveNode::Make(name, Repetition::REQUIRED, Type::INT64, ConvertedType::UINT_64);
+}
+
+parquet::schema::NodePtr String(const std::string& name) {
+    using namespace parquet;
+    return schema::PrimitiveNode::Make(name, Repetition::REQUIRED, Type::BYTE_ARRAY, ConvertedType::UTF8);
+}
+
+struct Link {
+    uint64_t path_id, upstream, downstream;
+
+    static std::shared_ptr<parquet::schema::GroupNode> GetSchema() {
+        using namespace parquet;
+        schema::NodeVector fields{UInt64("path_id"), UInt64("upstream"), UInt64("downstream")};
+        auto group_node = schema::GroupNode::Make("schema", Repetition::REQUIRED, fields);
+        return std::static_pointer_cast<schema::GroupNode>(group_node);
+    }
+};
+
+parquet::StreamWriter& operator<<(parquet::StreamWriter& os, const Link& link) {
+    os << link.path_id << link.upstream << link.downstream << parquet::EndRow;
+    return os;
+}
+
+struct PathBin {
+    uint64_t path_id;
+    uint64_t bin_id;
+    algorithms::path_info_t path_info;
+
+    std::vector<std::pair<uint64_t, uint64_t>> ranges() {
+        return {{path_info.first_nucleotide, path_info.last_nucleotide}};
+    }
+
+    static std::shared_ptr<parquet::schema::GroupNode> GetSchema() {
+        using namespace parquet;
+        schema::NodeVector fields;
+        fields.push_back(UInt64("path_id"));
+        fields.push_back(UInt64("bin_id"));
+        fields.push_back(schema::Double("mean_cov"));
+        fields.push_back(schema::Double("mean_inv"));
+        fields.push_back(schema::Double("mean_pos"));
+        auto group_node = schema::GroupNode::Make("schema", Repetition::REQUIRED, fields);
+        return std::static_pointer_cast<schema::GroupNode>(group_node);
+    }
+};
+
+parquet::StreamWriter& operator<<(parquet::StreamWriter& os, const PathBin& bin) {
+    os << bin.path_id
+       << bin.bin_id
+       << bin.path_info.mean_cov
+       << bin.path_info.mean_inv
+       << bin.path_info.mean_pos
+       << parquet::EndRow;
+    return os;
+}
+
+struct Path {
+    uint64_t path_id;
+    std::string name, prefix, suffix;
+
+    static std::shared_ptr<parquet::schema::GroupNode> GetSchema() {
+        using namespace parquet;
+        schema::NodeVector fields;
+        fields.push_back(UInt64("path_id"));
+        fields.push_back(String("name"));
+        fields.push_back(String("prefix"));
+        fields.push_back(String("suffix"));
+        auto group_node = schema::GroupNode::Make("schema", Repetition::REQUIRED, fields);
+        return std::static_pointer_cast<schema::GroupNode>(group_node);
+    }
+};
+
+parquet::StreamWriter& operator<<(parquet::StreamWriter& os, const Path& path) {
+    os << path.path_id << path.name << path.prefix << path.suffix << parquet::EndRow;
+    return os;
+}
+
+struct PathBinRange {
+    uint64_t path_id, bin_id, start, end;
+
+    static std::shared_ptr<parquet::schema::GroupNode> GetSchema() {
+        using namespace parquet;
+        schema::NodeVector fields;
+        fields.push_back(UInt64("path_id"));
+        fields.push_back(UInt64("bin_id"));
+        fields.push_back(UInt64("start"));
+        fields.push_back(UInt64("end"));
+        auto group_node = schema::GroupNode::Make("schema", Repetition::REQUIRED, fields);
+        return std::static_pointer_cast<schema::GroupNode>(group_node);
+    }
+};
+
+parquet::StreamWriter& operator<<(parquet::StreamWriter& os, const PathBinRange& pbr) {
+    os << pbr.path_id << pbr.bin_id << pbr.start << pbr.end << parquet::EndRow;
+    return os;
+}
+
+struct Metadata {
+    std::string key, value;
+
+    static std::shared_ptr<parquet::schema::GroupNode> GetSchema() {
+        using namespace parquet;
+        schema::NodeVector fields{String("key"), String("value")};
+        auto group_node = schema::GroupNode::Make("schema", Repetition::REQUIRED, fields);
+        return std::static_pointer_cast<schema::GroupNode>(group_node);
+    }
+};
+
+parquet::StreamWriter& operator<<(parquet::StreamWriter& os, const Metadata& metadata) {
+    os << metadata.key << metadata.value << parquet::EndRow;
+    return os;
+}
+
+class ParquetSerializer : public algorithms::BinSerializer {
+    parquet::StreamWriter paths_;
+    parquet::StreamWriter path_bins_;
+    parquet::StreamWriter path_bin_ranges_;
+    parquet::StreamWriter links_;
+
+    uint64_t path_id;
+    std::filesystem::path output_dir_;
+
+    static parquet::StreamWriter create_writer(const std::string& filename,
+                                               const std::shared_ptr<parquet::schema::GroupNode>& schema) {
+        std::shared_ptr<arrow::io::FileOutputStream> outfile;
+        PARQUET_ASSIGN_OR_THROW(outfile, arrow::io::FileOutputStream::Open(filename));
+
+        parquet::StreamWriter w{parquet::ParquetFileWriter::Open(outfile, schema)};
+        w.SetMaxRowGroupSize(1000000);
+        return w;
+    }
+
+    static const int ODGI_PARQUET_VERSION = 1;
+
+public:
+    ParquetSerializer(const std::string& path_delim, bool aggregate_delim, const std::filesystem::path& output_dir) :
+        algorithms::BinSerializer(path_delim, aggregate_delim),
+        path_id(0), output_dir_(output_dir)
+    {
+        std::filesystem::create_directories(output_dir);
+
+        paths_ = create_writer(output_dir / "paths.parquet", Path::GetSchema());
+        path_bins_ = create_writer(output_dir / "path_bins.parquet", PathBin::GetSchema());
+        path_bin_ranges_ = create_writer(output_dir / "path_bin_ranges.parquet", PathBinRange::GetSchema());
+        links_ = create_writer(output_dir / "links.parquet", Link::GetSchema());
+    }
+
+    void write_header(const uint64_t pangenome_length, const uint64_t bin_width) override {
+        auto w = create_writer(output_dir_ / "metadata.parquet", Metadata::GetSchema());
+        w << Metadata{"ODGI_PARQUET_VERSION", std::to_string(ODGI_PARQUET_VERSION)};
+        w << Metadata{"pangenome_length", std::to_string(pangenome_length)};
+        w << Metadata{"bin_width", std::to_string(bin_width)};
+    }
+
+    void write_seq(const uint64_t& bin_id, const std::string& seq) override {}
+
+    void write_path(const std::string& name, const link_vec_t& links, const bin_map_t &bins) override {
+        paths_ << Path{path_id, name, get_path_prefix(name), get_path_suffix(name)};
+
+        for (auto& bin: bins) {
+            PathBin path_bin{path_id, bin.first, bin.second};
+            path_bins_ << path_bin;
+            for (const auto& range: path_bin.ranges())
+                path_bin_ranges_ << PathBinRange{path_id, bin.first, range.first, range.second};
+        }
+
+        for (auto& link: links)
+            links_ << Link{path_id, link.first, link.second};
+
+        ++path_id;
+    }
+};
+
 int main_bin(int argc, char** argv) {
 
     for (uint64_t i = 1; i < argc-1; ++i) {
@@ -120,11 +300,11 @@ int main_bin(int argc, char** argv) {
 
     args::ArgumentParser parser("binning of path information in the graph");
     args::HelpFlag help(parser, "help", "display this help summary", {'h', "help"});
-    args::ValueFlag<std::string> dg_out_file(parser, "FILE", "store the graph in this file", {'o', "out"});
     args::ValueFlag<std::string> dg_in_file(parser, "FILE", "load the graph from this file", {'i', "idx"});
     args::ValueFlag<std::string> fa_out_file(parser, "FILE", "store the pangenome sequence in FASTA format in this file", {'f', "fasta"});
     args::ValueFlag<std::string> path_delim(parser, "path-delim", "annotate rows by prefix and suffix of this delimiter", {'D', "path-delim"});
     args::Flag output_json(parser, "write-json", "write JSON format output including additional path positional information", {'j', "json"});
+    args::ValueFlag<std::string> parquet_out_dir(parser, "DIR", "write Parquet format output to this directory", {'p', "parquet"});
     args::Flag aggregate_delim(parser, "aggregate-delim", "aggregate on path prefix delimiter", {'a', "aggregate-delim"});
     args::ValueFlag<uint64_t> num_bins(parser, "N", "number of bins", {'n', "num-bins"});
     args::ValueFlag<uint64_t> bin_width(parser, "bp", "width of each bin in basepairs along the graph vector", {'w', "bin-width"});
@@ -188,6 +368,8 @@ int main_bin(int argc, char** argv) {
     bool skip_seqs = args::get(write_seqs_not) || fa_out_file;
     if (args::get(output_json)) {
         serializer = std::make_shared<JsonSerializer>(delim, agg_delim, !skip_seqs);
+    } else if (parquet_out_dir) {
+        serializer = std::make_shared<ParquetSerializer>(delim, agg_delim, args::get(parquet_out_dir));
     } else {
         serializer = std::make_shared<TsvSerializer>(delim, agg_delim);
     }
