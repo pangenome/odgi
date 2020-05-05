f8c6574a21d3e0c368cff0dc39b7e4bc3b2e6abf
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-05-05 22:22:16 +0200

    Follow @mr-c idea of having a stripped binary, but also have a debug image. Easy script added to build all 3 images at once

--- docker/buildImages.sh
+++ docker/buildImages.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 user=$1
-docker build --target binary -t ${user}/odgi:latest .
-docker build --target python3 -t ${user}/odgi-python3:latest .
+repository=$2
+if [ -z $repository ]
+then
+    repository=localhost
+fi
+docker build --target binary -t ${repository}/${user}/odgi:latest .
+docker build --target debug -t ${repository}/${user}/odgi-debug:latest .
+docker build --target python3 -t ${repository}/${user}/odgi-python3:latest .
