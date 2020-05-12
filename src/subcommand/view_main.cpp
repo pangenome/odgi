1ab3fb33e04c492fc1e526d6730aa57185309652
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2020-05-12 14:50:11 +0200

    kmers, unitig, view, viz

--- src/subcommand/view_main.cpp
+++ src/subcommand/view_main.cpp
@@ -39,6 +39,11 @@ int main_view(int argc, char** argv) {
         return 1;
     }
 
+    if (!dg_in_file) {
+        std::cerr << "Please specify an input file to load the graph via -i=[FILE], --idx=[FILE]." << std::endl;
+        return 1;
+    }
+
     graph_t graph;
     assert(argc > 0);
     std::string infile = args::get(dg_in_file);
