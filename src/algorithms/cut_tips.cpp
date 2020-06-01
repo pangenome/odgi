5036ab070af9930b310d963046f84919dd9786f7
Erik Garrison <erik.garrison@gmail.com>
2020-06-02 01:16:35 +0200

    remove tips while trimming paths

--- src/algorithms/cut_tips.cpp
+++ src/algorithms/cut_tips.cpp
@@ -13,6 +13,7 @@ std::vector<handle_t> tip_handles(
         [&tips,&graph](const handle_t& handle) {
             // check if one end of the handle is a tip
             if (graph.get_degree(handle, false) == 0
+                // might be more correct if this were xor
                 || graph.get_degree(handle, true) == 0) {
                 tips.push_back(handle);
             }
@@ -32,17 +33,25 @@ uint64_t cut_tips(
 uint64_t cut_tips(
     MutablePathDeletableHandleGraph& graph) {
     auto tips = tip_handles(graph);
+    std::sort(tips.begin(), tips.end());
     for (auto& tip : tips) {
-        //graph.destroy_handle(tip);
         std::vector<step_handle_t> to_destroy;
         graph.for_each_step_on_handle(
             tip,
             [&](const step_handle_t& step) {
                 to_destroy.push_back(step);
             });
+        // odgi quirk / baddness: we have to destroy step handles from largest to smallest
+        // a generic implementation would iterate through step handles while erasing
+        std::sort(to_destroy.begin(), to_destroy.end(),
+                  [](const step_handle_t& a, const step_handle_t& b) {
+                      return as_integers(a)[1] > as_integers(b)[1]; });
         for (auto& step : to_destroy) {
-            graph.destroy_step(step);
+            graph.rewrite_segment(step, step, {});
         }
+    }
+    for (auto& tip : tips) {
+        graph.destroy_handle(tip);
     }
     return tips.size();
 }
