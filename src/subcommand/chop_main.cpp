8f9e35eb12a46280a5f41edd9d08183911b8dd37
AndreaGuarracino <andreaguarracino@outlook.com>
2020-09-04 11:46:32 +0200

    updated odgi chop documentation

--- src/subcommand/chop_main.cpp
+++ src/subcommand/chop_main.cpp
@@ -23,7 +23,7 @@ int main_chop(int argc, char** argv) {
     args::ValueFlag<std::string> dg_in_file(parser, "FILE", "load the graph from this file", {'i', "idx"});
     args::ValueFlag<std::string> dg_out_file(parser, "FILE", "store the graph self index in this file", {'o', "out"});
     args::ValueFlag<uint64_t> chop_to(parser, "N", "divide nodes to be shorter than this length", {'c', "chop-to"});
-    args::ValueFlag<uint64_t> nthreads(parser, "N", "number of threads to use for parallel sorter", {'t', "threads"});
+    args::ValueFlag<uint64_t> nthreads(parser, "N", "number of threads to use for the parallel sorter", {'t', "threads"});
     args::Flag debug(parser, "debug", "print information about the components", {'d', "debug"});
 
     try {
@@ -86,7 +86,7 @@ int main_chop(int argc, char** argv) {
     return 0;
 }
 
-static Subcommand odgi_chop("chop", "chop long nodes into short ones while preserving topology",
+static Subcommand odgi_chop("chop", "chop long nodes into short ones while preserving topology and node order",
                             PIPELINE, 3, main_chop);
 
 
