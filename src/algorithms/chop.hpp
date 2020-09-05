dd3fc247daf4c3735e3ce156b95bdc04515c3914
AndreaGuarracino <andreaguarracino@outlook.com>
2020-09-05 09:57:58 +0200

    little change in the chop interface

--- src/algorithms/chop.hpp
+++ src/algorithms/chop.hpp
@@ -17,7 +17,7 @@ using namespace handlegraph;
  * Cut nodes to be less than the given max node length.
  */
 void chop(handlegraph::MutablePathDeletableHandleGraph& graph, const uint64_t& max_node_length,
-          const uint64_t& nthreads, const bool show_info);
+          const uint64_t& nthreads, const bool& show_info);
     
 }
 }
