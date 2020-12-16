92219963dc12ba98f8d04daca386f0523fa8f516
Erik Garrison <erik.garrison@gmail.com>
2020-12-16 20:59:16 +0100

    test demonstrating path iteration failure

--- src/unittest/simplify.cpp
+++ src/unittest/simplify.cpp
@@ -185,5 +185,60 @@ TEST_CASE("Graph simplification reduces a graph with a self inverting -/+ loop",
     }
 }
 
+TEST_CASE("Graph simplification reduces a graph with a self inverting -/+ loop with paths", "[simplify]") {
+    graph_t graph;
+    handle_t n1 = graph.create_handle("CAAATAAG");
+    handle_t n2 = graph.create_handle("A");
+    handle_t n3 = graph.create_handle("G");
+    handle_t n4 = graph.create_handle("T");
+    handle_t n5 = graph.create_handle("C");
+    handle_t n6 = graph.create_handle("TTG");
+    graph.create_edge(n1, n2);
+    graph.create_edge(n2, n3);
+    graph.create_edge(graph.flip(n3), n3);
+    graph.create_edge(n3, n4);
+    graph.create_edge(n4, n5);
+    graph.create_edge(n5, n6);
+    path_handle_t p_x = graph.create_path_handle("x");
+    path_handle_t p_y = graph.create_path_handle("y");
+    for (auto& p : { p_x, p_y }) {
+        for (auto& h : { n1, n2, n3, n4, n5, n6 }) {
+            graph.append_step(p, h);
+        }
+    }
+    path_handle_t q_y = graph.create_path_handle("q");
+    for (auto& p : { q_y }) {
+        for (auto& h : { n6, n5, n4, n3, n2, n1 }) {
+            graph.append_step(p, graph.flip(h));
+        }
+    }
+
+    algorithms::unchop(graph);
+    graph.to_gfa(std::cerr);
+
+    for (auto& p : { p_x, p_y, q_y }) {
+        step_handle_t begin = graph.path_begin(p);
+        //std::cerr << "begin is " << graph.get_id(graph.get_handle_of_step(begin)) << std::endl;
+        step_handle_t end = graph.path_end(p);
+        //std::cerr << "end is " << graph.get_id(graph.get_handle_of_step(end)) << std::endl;
+        for (step_handle_t step = begin;
+             step != end;
+             step = graph.get_next_step(step)) {
+            handle_t h = graph.get_handle_of_step(step);
+            //std::cerr << "on step " << graph.get_id(h) << std::endl;
+            //internal_nodes.insert(consensus->get_id(h));
+        }
+    }
+
+    // sort the graph
+    graph.apply_ordering(algorithms::topological_order(&graph), true);
+    SECTION("The graph is as expected") {
+        REQUIRE(graph.get_sequence(graph.get_handle(1)) == "CAAATAAGA");
+        REQUIRE(graph.get_sequence(graph.get_handle(2)) == "GTCTTG");
+        REQUIRE(graph.has_edge(graph.get_handle(1), graph.get_handle(2)));
+        REQUIRE(graph.has_edge(graph.flip(graph.get_handle(2)), graph.get_handle(2)));
+    }
+}
+
 }
 }
