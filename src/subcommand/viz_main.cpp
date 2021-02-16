3a07bb9cd4c61c6ffff38f1555bd34d46d11b59c
AndreaGuarracino <andreaguarracino@outlook.com>
2021-02-16 17:41:28 +0100

    handling of possible errors

--- src/subcommand/viz_main.cpp
+++ src/subcommand/viz_main.cpp
@@ -20,6 +20,9 @@
 #define PATH_NAMES_MAX_NUM_OF_CHARACTERS 128
 #define PATH_NAMES_MAX_CHARACTER_SIZE 64
 
+bool is_number(const std::string &s) {
+    return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
+}
 
 namespace odgi {
 
@@ -58,7 +61,7 @@ namespace odgi {
         args::ValueFlag<char> _color_by_prefix(parser, "C", "colors paths by their names looking at the prefix before the given character C",{'s', "color-by-prefix"});
 
         /// Range selection
-        args::ValueFlag<std::string> _nucleotide_pangenomic_range(parser, "STRING","nucleotide pangenomic range to visualize [pos1-pos2]",{'r', "pangenomic-range"});
+        args::ValueFlag<std::string> _nucleotide_pangenomic_range(parser, "STRING","nucleotide pangenomic range to visualize: STRING=start-end. The nucleotide positions refer to the pangenome's sequence (i.e., the sequence obtained arranging all the graph's node from left to right).",{'r', "pangenomic-range"});
 
         /// Paths selection
         args::ValueFlag<std::string> path_names_file(parser, "FILE", "list of paths to display in the specified order; the file must contain one path name per line and a subset of all paths can be specified.", {'p', "paths-to-display"});
@@ -216,15 +219,18 @@ namespace odgi {
 
 
                 if (splitted.size() != 2) {
-                    //todo to improve the message
                     std::cerr
-                            << "[odgi viz] error: Please specify a nucleotide pangenomic range with a start position "
-                               "and an end position."
+                            << "[odgi viz] error: Please specify a valid nucleotide pangenomic range: STRING=start-end."
                             << std::endl;
                     return 1;
                 }
 
-                //todo checks that they are numbers
+                if (!is_number(splitted[0]) || !is_number(splitted[1])) {
+                    std::cerr
+                            << "[odgi viz] error: Please specify valid numbers for the nucleotide pangenomic range."
+                            << std::endl;
+                    return 1;
+                }
 
                 pangenomic_start_pos = std::min((double)len - 1, (double)stoi(splitted[0]));
                 pangenomic_end_pos = std::min((double)len - 1, (double)stoi(splitted[1]));
