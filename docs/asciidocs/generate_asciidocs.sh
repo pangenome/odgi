3a62e72bc2ce05545a76c27b6c64e195328ae37f
AndreaGuarracino <andreaguarracino@outlook.com>
2021-03-06 12:31:40 +0100

    changed name from explode to squeeze; followed the convention of using `-P \--progress` in odgi cover, implode, squeeze

--- docs/asciidocs/generate_asciidocs.sh
+++ docs/asciidocs/generate_asciidocs.sh
@@ -37,7 +37,7 @@ asciidoctor -b manpage odgi_test.adoc
 asciidoctor -b manpage odgi_version.adoc
 asciidoctor -b manpage odgi_cover.adoc
 asciidoctor -b manpage odgi_explode.adoc
-asciidoctor -b manpage odgi_implode.adoc
+asciidoctor -b manpage odgi_squeeze.adoc
 asciidoctor -b manpage odgi_position.adoc
 
 mv *.1 man/
