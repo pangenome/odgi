c42c3872c67a177bdbbc31d37aed0581bc5a2c4b
AndreaGuarracino <andreaguarracino@outlook.com>
2021-04-03 12:27:49 +0200

    parallelized

--- src/subcommand/validate_main.cpp
+++ src/subcommand/validate_main.cpp
@@ -66,16 +66,24 @@ namespace odgi {
 
         bool valid_graph = true;
 
+        std::vector<path_handle_t> paths;
+        paths.reserve(graph.get_path_count());
         graph.for_each_path_handle([&](const path_handle_t &path) {
-            graph.for_each_step_in_path(path, [&](const step_handle_t& step) {
+            paths.push_back(path);
+        });
+
+#pragma omp parallel for schedule(dynamic, 1) num_threads(num_threads)
+        for (auto path : paths) {
+            graph.for_each_step_in_path(path, [&](const step_handle_t &step) {
                 if (graph.has_next_step(step)) {
                     step_handle_t next_step = graph.get_next_step(step);
                     handle_t h = graph.get_handle_of_step(step);
                     handle_t next_h = graph.get_handle_of_step(next_step);
 
                     if (!graph.has_edge(h, next_h)) {
-                        std::cerr << "[odgi::validate] error: path " << graph.get_path_name(path) << " does not respect "
-                                  << "the graph topology. The link "
+#pragma omp critical (cout)
+                        std::cerr << "[odgi::validate] error: the path " << graph.get_path_name(path) << " does not "
+                                  << "respect the graph topology: the link "
                                   << graph.get_id(h) << (graph.get_is_reverse(h) ? "-" : "+")
                                   << ","
                                   << graph.get_id(next_h) << (graph.get_is_reverse(next_h) ? "-" : "+")
@@ -85,30 +93,7 @@ namespace odgi {
                     }
                 }
             });
-        });
-
-
-//        nid_t prev_id = source.get_id(prev);
-//        if (subgraph.has_node(prev_id)) {
-//            handle_t subgraph_prev = subgraph.get_handle(prev_id, source.get_is_reverse(prev));
-//            if (!subgraph.has_edge(subgraph_prev, handle)) {
-//                subgraph.create_edge(subgraph_prev, handle);
-//            }
-//        }
-
-//        for (path_handle_t path_handle : paths) {
-//            path_handle_t new_path_handle = component.create_path_handle(source.get_path_name(path_handle),
-//                                                                         source.get_is_circular(path_handle));
-//
-//            auto path_end = source.path_end(path_handle);
-//            for (step_handle_t cur_step = source.path_begin(path_handle);
-//                 cur_step != path_end; cur_step = source.get_next_step(cur_step)) {
-//                handle_t cur_handle = source.get_handle_of_step(cur_step);
-//
-//                component.append_step(new_path_handle, component.get_handle(source.get_id(cur_handle),
-//                                                                            source.get_is_reverse(cur_handle)));
-//            }
-//        }
+        }
 
         return (valid_graph ? 0 : 1);
     }
