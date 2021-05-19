00d68c282b5dea48590316705cb2fa0fd85a9cf2
AndreaGuarracino <andreaguarracino@outlook.com>
2021-05-19 16:33:33 +0200

    remove builded doc [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -20,13 +20,13 @@ sys.path.insert(0, os.path.abspath('../lib/'))
 # -- Project information -----------------------------------------------------
 
 project = u'odgi'
-copyright = '2021, Erik Garrison. Revision v0.5.1-a45139f'
+copyright = '2021, Erik Garrison. Revision v0.5.1-f75fafa'
 author = u'Andrea Guarracino, Simon Heumos, ... , Pjotr Prins, Erik Garrison'
 
 # The short X.Y version
 version = 'v0.5.1'
 # The full version, including alpha/beta/rc tags
-release = 'a45139f'
+release = 'f75fafa'
 
 
 # -- General configuration ---------------------------------------------------
