a344ddd0aa076a7925fa3e353d17bce5994ddd56
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2021-06-04 14:45:05 +0200

    add a group of mandatory arguments

--- src/subcommand/build_main.cpp
+++ src/subcommand/build_main.cpp
@@ -21,10 +21,11 @@ int main_build(int argc, char** argv) {
     --argc;
     
     args::ArgumentParser parser("Construct a dynamic succinct variation graph in ODGI format from a GFAv1.");
-    args::Group graph_files_io(parser, "[ Graph Files IO ]");
-    args::ValueFlag<std::string> gfa_file(graph_files_io, "FILE", "GFAv1 FILE containing the nodes, edges and "
+    args::Group mandatory(parser, "[ MANDATORY OPTIONS ]");
+    args::ValueFlag<std::string> gfa_file(mandatory, "FILE", "GFAv1 FILE containing the nodes, edges and "
                                                           "paths to build a dynamic succinct variation graph from.", {'g', "gfa"});
-    args::ValueFlag<std::string> dg_out_file(graph_files_io, "FILE", "Write the dynamic succinct variation graph to this file. A file ending with *.og* is recommended.", {'o', "out"});
+    args::ValueFlag<std::string> dg_out_file(mandatory, "FILE", "Write the dynamic succinct variation graph to this file. A file ending with *.og* is recommended.", {'o', "out"});
+    args::Group graph_files_io(parser, "[ Graph Files IO ]");
     args::Flag to_gfa(graph_files_io, "to_gfa", "Write the graph to stdout in GFAv1 format.", {'G', "to-gfa"});
     args::Group graph_sorting(parser, "[ Graph Sorting ]");
     args::Flag toposort(graph_sorting, "sort", "Apply a general topological sort to the graph and order the node ids"
@@ -33,10 +34,12 @@ int main_build(int argc, char** argv) {
     args::Group threading(parser, "[ Threading ]");
     args::ValueFlag<uint64_t> nthreads(threading, "N", "Number of threads to use for parallel operations.", {'t', "threads"});
     args::Group program_information(parser, "[ Program Information ]");
-    args::Flag progress(program_information, "progress", "show progress updates", {'P', "progress"});
+    args::Flag progress(program_information, "progress", "Write the current progress to stderr.", {'P', "progress"});
     args::HelpFlag help(program_information, "help", "Display this help summary.", {'h', "help"});
     args::Group processing_information(parser, "[ Processing Information ]");
-    args::Flag debug(processing_information, "debug", "enable debugging", {'d', "debug"});
+    args::Flag debug(processing_information, "debug", "Verbosely print graph information to stderr. This includes the maximum"
+                                                      "  node_id, the minimum node_id, the handle to node_id mapping, the"
+                                                      "  deleted nodes and the path metadata.", {'d', "debug"});
     try {
         parser.ParseCLI(argc, argv);
     } catch (args::Help) {
@@ -52,14 +55,6 @@ int main_build(int argc, char** argv) {
         return 1;
     }
 
-    /*
-    size_t n_threads = args::get(num_threads);
-    if (n_threads) {
-        omp_set_num_threads(args::get(num_threads));
-    } else {
-        omp_set_num_threads(1);
-    }
-    */
     graph_t graph;
     
     //make_graph();
