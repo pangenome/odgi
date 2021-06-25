246972a0c2eb03d112c124d89d80028615425b47
Erik Garrison <erik.garrison@gmail.com>
2021-06-25 13:20:23 +0200

    use a work queue to avoid recursion

--- src/algorithms/untangle.cpp
+++ src/algorithms/untangle.cpp
@@ -7,114 +7,115 @@ using namespace handlegraph;
 
 std::vector<step_handle_t> untangle_cuts(
     const PathHandleGraph& graph,
-    const step_handle_t& start,
-    const step_handle_t& end,
+    const step_handle_t& _start,
+    const step_handle_t& _end,
     const ska::flat_hash_map<step_handle_t, uint64_t>& step_pos) {
-    auto path = graph.get_path_handle_of_step(start);
+    auto path = graph.get_path_handle_of_step(_start);
     auto path_name = graph.get_path_name(path);
     // this assumes that the end is not inclusive
-    uint64_t start_pos = step_pos.find(start)->second;
-    uint64_t end_pos = step_pos.find(end)->second;
     /*
     std::cerr << "untangle_cuts(" << path_name << ", "
               << start_pos << ", "
               << end_pos << ")" << std::endl;
     */
     std::vector<step_handle_t> cut_points;
-    cut_points.push_back(start);
-    // we go forward until we see a loop, where the other step has position < end_pos and > start_pos
-    for (step_handle_t step = start; step != end; step = graph.get_next_step(step)) {
-        //  we take the first and shortest loop we find
-        // TODO change this, it can be computed based on the node length
-        const auto& curr_pos = step_pos.find(step)->second;
-        handle_t handle = graph.get_handle_of_step(step);
-        bool found_loop = false;
-        step_handle_t other;
-        graph.for_each_step_on_handle(
-            handle,
-            [&](const step_handle_t& s) {
-                if (step != s // not the step we're on
-                    && graph.get_path_handle_of_step(s) == path) {
-                    const auto& other_pos = step_pos.find(s)->second;
-                    if (other_pos > start_pos
-                        && other_pos < end_pos
-                        && (!found_loop && other_pos > curr_pos
-                            || (found_loop && (step_pos.find(other)->second > other_pos)))) {
-                        found_loop = true;
-                        other = s;
+    std::deque<std::pair<step_handle_t, step_handle_t>> todo;
+    todo.push_back(std::make_pair(_start, _end));
+    while (!todo.empty()) {
+        auto& start = todo.front().first;
+        auto& end = todo.front().second;
+        uint64_t start_pos = step_pos.find(start)->second;
+        uint64_t end_pos = step_pos.find(end)->second;
+        todo.pop_front();
+        cut_points.push_back(start);
+        // we go forward until we see a loop, where the other step has position < end_pos and > start_pos
+        for (step_handle_t step = start; step != end; step = graph.get_next_step(step)) {
+            //  we take the first and shortest loop we find
+            // TODO change this, it can be computed based on the node length
+            const auto& curr_pos = step_pos.find(step)->second;
+            handle_t handle = graph.get_handle_of_step(step);
+            bool found_loop = false;
+            step_handle_t other;
+            graph.for_each_step_on_handle(
+                handle,
+                [&](const step_handle_t& s) {
+                    if (step != s // not the step we're on
+                        && graph.get_path_handle_of_step(s) == path) {
+                        const auto& other_pos = step_pos.find(s)->second;
+                        if (other_pos > start_pos
+                            && other_pos < end_pos
+                            && (!found_loop && other_pos > curr_pos
+                                || (found_loop && (step_pos.find(other)->second > other_pos)))) {
+                            found_loop = true;
+                            other = s;
+                        }
                     }
-                }
-            });
-        if (found_loop) {
-            //  recurse this function into it, taking start as our current handle other side of the loop as our end
-            //  to cut_points we add the start position, the result from recursion, and our end position
-            /*
-            if (step_pos.find(other)->second < step_pos.find(step)->second) {
-                std::cerr << "impossible" << std::endl;
-                abort();
+                });
+            if (found_loop) {
+                //  recurse this function into it, taking start as our current handle other side of the loop as our end
+                //  to cut_points we add the start position, the result from recursion, and our end position
+                /*
+                  if (step_pos.find(other)->second < step_pos.find(step)->second) {
+                  std::cerr << "impossible" << std::endl;
+                  abort();
+                  }
+                */
+                todo.push_back(std::make_pair(step, other));
+                //  we then step forward to the loop end and continue iterating
+                step = other;
             }
-            */
-            auto internal_cuts = untangle_cuts(graph, step, other, step_pos);
-            cut_points.insert(cut_points.end(),
-                              internal_cuts.begin(),
-                              internal_cuts.end());
-            //  we then step forward to the loop end and continue iterating
-            step = other;
         }
-    }
-    // TODO this block is the same as the previous algorithm, but in reverse
-    // the differences in how positions are managed are subtle, making it hard to fold the
-    // forward and reverse version together
-    // now we reverse it
-    step_handle_t path_begin = graph.path_begin(path);
-    if (end == path_begin || !graph.has_previous_step(end)) {
-        return cut_points;
-    }
-    //std::cerr << "reversing" << std::endl;
-    for (step_handle_t step = end;
-         step_pos.find(step)->second > start_pos;
-         step = graph.get_previous_step(step)) {
-        //  we take the first and shortest loop we find
-        // TODO change this, it can be computed based on the node length
-        const auto& curr_pos = step_pos.find(step)->second;
-        handle_t handle = graph.get_handle_of_step(step);
-        //std::cerr << "rev on step " << graph.get_id(handle) << " " << curr_pos << std::endl;
-        bool found_loop = false;
-        step_handle_t other;
-        graph.for_each_step_on_handle(
-            handle,
-            [&](const step_handle_t& s) {
-                if (step != s // not the step we're on
-                    && graph.get_path_handle_of_step(s) == path) {
-                    const auto& other_pos = step_pos.find(s)->second;
-                    if (other_pos > start_pos
-                        && other_pos < end_pos
-                        && other_pos < curr_pos
-                        && (!found_loop
-                            || (found_loop && (step_pos.find(other)->second < other_pos)))) {
-                        found_loop = true;
-                        other = s;
+        // TODO this block is the same as the previous algorithm, but in reverse
+        // the differences in how positions are managed are subtle, making it hard to fold the
+        // forward and reverse version together
+        // now we reverse it
+        step_handle_t path_begin = graph.path_begin(path);
+        if (end == path_begin || !graph.has_previous_step(end)) {
+            return cut_points;
+        }
+        //std::cerr << "reversing" << std::endl;
+        for (step_handle_t step = end;
+             step_pos.find(step)->second > start_pos;
+             step = graph.get_previous_step(step)) {
+            //  we take the first and shortest loop we find
+            // TODO change this, it can be computed based on the node length
+            const auto& curr_pos = step_pos.find(step)->second;
+            handle_t handle = graph.get_handle_of_step(step);
+            //std::cerr << "rev on step " << graph.get_id(handle) << " " << curr_pos << std::endl;
+            bool found_loop = false;
+            step_handle_t other;
+            graph.for_each_step_on_handle(
+                handle,
+                [&](const step_handle_t& s) {
+                    if (step != s // not the step we're on
+                        && graph.get_path_handle_of_step(s) == path) {
+                        const auto& other_pos = step_pos.find(s)->second;
+                        if (other_pos > start_pos
+                            && other_pos < end_pos
+                            && other_pos < curr_pos
+                            && (!found_loop
+                                || (found_loop && (step_pos.find(other)->second < other_pos)))) {
+                            found_loop = true;
+                            other = s;
+                        }
                     }
-                }
-            });
-        if (found_loop) {
-            //  recurse this function into it, taking start as our current handle other side of the loop as our end
-            //  to cut_points we add the start position, the result from recursion, and our end position
-            /*
-            if (step_pos.find(other)->second > step_pos.find(step)->second) {
-                std::cerr << "impossible" << std::endl;
-                abort();
+                });
+            if (found_loop) {
+                //  recurse this function into it, taking start as our current handle other side of the loop as our end
+                //  to cut_points we add the start position, the result from recursion, and our end position
+                /*
+                  if (step_pos.find(other)->second > step_pos.find(step)->second) {
+                  std::cerr << "impossible" << std::endl;
+                  abort();
+                  }
+                */
+                todo.push_back(std::make_pair(other, step));
+                //  we then step forward to the loop end and continue iterating
+                step = other;
             }
-            */
-            auto internal_cuts = untangle_cuts(graph, other, step, step_pos);
-            cut_points.insert(cut_points.end(),
-                              internal_cuts.begin(),
-                              internal_cuts.end());
-            //  we then step forward to the loop end and continue iterating
-            step = other;
         }
+        cut_points.push_back(end);
     }
-    cut_points.push_back(end);
     // and sort
     std::sort(cut_points.begin(),
               cut_points.end(),
