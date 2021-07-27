807cf8d6e2d265c1104530dfed5fb8357380ee7a
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2021-07-27 11:38:50 +0200

    fix a bug in depth_main

--- src/subcommand/depth_main.cpp
+++ src/subcommand/depth_main.cpp
@@ -480,7 +480,7 @@ namespace odgi {
 
             graph.for_each_handle(
                 [&](const handle_t& h) {
-                    auto id = graph.get_id(h) - shift;
+                    auto id = graph.get_id(h);
 
                     depths[id - shift] = get_graph_node_depth(graph, id, paths_to_consider).first;
                 }, true);
