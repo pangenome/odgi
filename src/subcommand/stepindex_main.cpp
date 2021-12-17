bca7d8b16f7177e0fba984e1ce50bdfe8e119a39
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2021-12-17 14:52:05 +0100

    stepindex.main interface ready

--- src/subcommand/stepindex_main.cpp
+++ src/subcommand/stepindex_main.cpp
@@ -20,15 +20,14 @@ namespace odgi {
 		--argc;
 
 		args::ArgumentParser parser(
-				"Generate a step index and access the position of each step of each path once. Prints the run time of each the index generation and the position fetching to stdout.");
+				"Generate a step index from a given graph. If no output file is provided via *-o, --out*, the index will be directly written to *INPUT_GRAPH.stpidx*.");
 		args::Group mandatory_opts(parser, "[ MANDATORY OPTIONS ]");
 		args::ValueFlag<std::string> og_file(mandatory_opts, "FILE", "Load the succinct variation graph in ODGI format from this *FILE*. The file name usually ends with *.og*. It also accepts GFAv1, but the on-the-fly conversion to the ODGI format requires additional time!", {'i', "input"});
 		args::Group step_index_opts(parser, "[ Step Index Options ]");
 		args::ValueFlag<uint64_t> _step_index_sample_rate(step_index_opts, "N", "The sample rate when building the step index. We index a node only if mod(node_id, step-index-sample-rate) == 0! Number must be dividable by 2. (default: 8).",
 														  {'a', "step-index-sample-rate"});
-		args::Flag naked_run(step_index_opts, "naked-run", "Only read in the given graph and then exit gracefully", {'n', "naked-run"});
-		args::ValueFlag<uint64_t> _iterations(step_index_opts, "N", "The number of position fetching rounds. For each path step a position is fetched N times. (default: 10).",
-														  {'b', "iterations"});
+		args::ValueFlag<std::string> dg_out_file(mandatory_opts, "FILE", "Write the created step index to the specified file. A file"
+																		 " ending with *.stpidx* is recommended. (default: *INPUT_GRAPH.stpidx*).", {'o', "out"});
 		args::Group threading(parser, "[ Threading ]");
 		args::ValueFlag<uint64_t> nthreads(threading, "N", "Number of threads to use for parallel operations.", {'t', "threads"});
 		args::Group processing_info_opts(parser, "[ Processing Information ]");
@@ -63,7 +62,7 @@ namespace odgi {
 			exit(1);
 		}
 
-		uint64_t iterations = args::get(_iterations) ? args::get(_iterations) : 10;
+		std::string step_index_out_file = dg_out_file ? args::get(dg_out_file) : (args::get(og_file) + ".stpidx");
 
 		const uint64_t num_threads = args::get(nthreads) ? args::get(nthreads) : 1;
 
@@ -78,11 +77,8 @@ namespace odgi {
 			}
 		}
 
-		if (naked_run) {
-			exit(0);
-		}
-
 		omp_set_num_threads(num_threads);
+
 
 		std::vector<path_handle_t> paths;
 		paths.reserve(graph.get_path_count());
@@ -90,63 +86,8 @@ namespace odgi {
 			paths.push_back(path);
 		});
 
-		auto distribute_seconds = [&](int& days, int& hours, int& minutes, int& seconds, const double& input_seconds) {
-			const int cseconds_in_day = 86400;
-			const int cseconds_in_hour = 3600;
-			const int cseconds_in_minute = 60;
-			const int cseconds = 1;
-			days = std::floor(input_seconds / cseconds_in_day);
-			hours = std::floor(((int)input_seconds % cseconds_in_day) / cseconds_in_hour);
-			minutes = std::floor((((int)input_seconds % cseconds_in_day) % cseconds_in_hour) / cseconds_in_minute);
-			seconds = ((((int)input_seconds % cseconds_in_day) % cseconds_in_hour) % cseconds_in_minute) / cseconds;
-		};
-
-		auto print_time = [&](const double& _seconds) {
-			int days = 0, hours = 0, minutes = 0, seconds = 0;
-			distribute_seconds(days, hours, minutes, seconds, _seconds);
-			std::stringstream buffer;
-			buffer << std::setfill('0') << std::setw(2) << days << ":"
-				   << std::setfill('0') << std::setw(2) << hours << ":"
-				   << std::setfill('0') << std::setw(2) << minutes << ":"
-				   << std::setfill('0') << std::setw(2) << seconds;
-			return buffer.str();
-		};
-
-		std::chrono::time_point<std::chrono::steady_clock> start_time_index_building = std::chrono::steady_clock::now();
 		algorithms::step_index_t step_index(graph, paths, num_threads, progress, step_index_sample_rate);
-		auto cur_time_index_building = std::chrono::steady_clock::now();
-		std::chrono::duration<double> elapsed_seconds_index_building = cur_time_index_building-start_time_index_building;
-		std::cout << "[odgi::stepindex] info: Building step index elapsed time: " << print_time(elapsed_seconds_index_building.count()) << std::endl;
-
-		std::chrono::time_point<std::chrono::steady_clock> start_time_position_fetching = std::chrono::steady_clock::now();
-
-		std::unique_ptr<algorithms::progress_meter::ProgressMeter> progress_meter;
-		if (progress) {
-			progress_meter = std::make_unique<algorithms::progress_meter::ProgressMeter>(paths.size()*iterations, "[odgi::stepindex::position_fetching] Progress:");
-		}
-
-		step_index.save("overlap_test_step_index");
-		algorithms::step_index_t step_index_1;
-		step_index_1.load("overlap_test_step_index");
-
-		for (int i = 0; i < iterations; i++) {
-#pragma omp parallel for schedule(dynamic, 1) num_threads(num_threads)
-			for (auto path: paths) {
-				std::cout << graph.get_path_name(path) << ": " << step_index_1.get_path_len(path) << std::endl;
-				graph.for_each_step_in_path(path, [&](const step_handle_t &step) {
-					std::cout << step_index_1.get_position(step, graph) << std::endl;
-				});
-				if (progress) {
-					progress_meter->increment(1);
-				}
-			}
-		}
-		if (progress) {
-			progress_meter->finish();
-		}
-		auto cur_time_position_fetching = std::chrono::steady_clock::now();
-		std::chrono::duration<double> elapsed_seconds_position_fetching = cur_time_position_fetching-start_time_position_fetching;
-		std::cout << "[odgi::stepindex] info: Position fetching elapsed time: " << print_time(elapsed_seconds_position_fetching.count()) << std::endl;
+		step_index.save(step_index_out_file);
 
 		return 0;
 	}
