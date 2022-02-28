8c26bfaf0d2b93a285be16b7903b15b5f970c4f3
Pjotr Prins <pjotr.public01@thebird.nl>
2022-02-28 05:42:55 -0600

    Started on C-API for FFIs

---
+++ src/odgi-api.cpp
@@ -0,0 +1,7 @@
+// odgi-api exports the C-API for FFIs
+//
+// Copyright (c) 2022 Erik Garrison and Pjotr Prins
+//
+// ODGI is published under the MIT license
+
+#include "odgi.hpp"
---
+++ src/odgi-api.h
@@ -0,0 +1,7 @@
+// odgi-api.h ODGI C-API for FFIs
+//
+// Copyright (c) 2022 Erik Garrison and Pjotr Prins
+//
+// ODGI is published under the MIT license
+
+#pragma once
