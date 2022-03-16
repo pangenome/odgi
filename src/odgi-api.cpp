4047a06d0c0e440e2c3422a2cfc3bd5178f2fdc0
Pjotr Prins <pjotr.public01@thebird.nl>
2022-03-16 09:44:44 +0100

    odgi-ffi: started on C-API
    - Pass path_handle_t as int
    - Iteratee supported to go through handles

--- src/odgi-api.cpp
+++ src/odgi-api.cpp
@@ -43,15 +43,37 @@ extern "C" {
     return Version::get_version().c_str();
   }
 
-  void odgi_for_each_path_handle(const graph_t *graph, void (*next) (int i, const path_handle_t &path)) {
-    int i = 0;
+  void odgi_for_each_path_handle(const graph_t *graph, void (*next) (const path_handle_t path)) {
     graph->for_each_path_handle([&](const path_handle_t& path) {
-      next(i,path);
-      i++;
+      next(path);
     });
   }
 
-  const char *odgi_get_path_name(const graph_t *graph, const path_handle_t& path) {
+  const bool odgi_for_each_handle(const graph_t *graph,
+                                  // const std::function<bool(const handlegraph::handle_t&)>& iteratee)
+                                  bool (*next) (const handlegraph::handle_t handle))
+  {
+    return graph->for_each_handle([&](const handlegraph::handle_t h)
+    {
+      next(h);
+      return true;
+    },
+      false); // note that parallel is false
+  }
+
+  const char *odgi_get_sequence(const graph_t *graph, const handle_t handle) {
+    return graph->get_sequence(handle).c_str();
+  }
+
+  const char *odgi_get_path_name(const graph_t *graph, const path_handle_t path) {
     return graph->get_path_name(path).c_str();
+  }
+
+  const bool odgi_has_path(const graph_t *graph, const char *path_name) {
+    return graph->has_path(path_name);
+  }
+
+  const path_handle_t odgi_get_path_handle(const graph_t *graph, const char *path_name) {
+    return graph->get_path_handle(path_name);
   }
 }
