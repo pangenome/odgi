083367f43b6add0591692ea60934b234cb2a502e
AndreaGuarracino <andreaguarracino@outlook.com>
2022-03-17 22:23:46 +0100

    update `odgi pav` tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -20,13 +20,13 @@ sys.path.insert(0, os.path.abspath('../lib/'))
 # -- Project information -----------------------------------------------------
 
 project = u'odgi'
-copyright = '2021, Guarracino A., Heumos S., Nahnsen S., Prins P., Garrison E. Revision v0.6.3-88cb049'
+copyright = '2021, Guarracino A., Heumos S., Nahnsen S., Prins P., Garrison E. Revision v0.6.3-da7dda4'
 author = u'Andrea Guarracino, Simon Heumos, Sven Nahnsen , Pjotr Prins, Erik Garrison'
 
 # The short X.Y version
 version = 'v0.6.3'
 # The full version, including alpha/beta/rc tags
-release = '88cb049'
+release = 'da7dda4'
 
 
 # -- General configuration ---------------------------------------------------
