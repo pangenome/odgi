5502907576a8300e7fd90ae4ef6045ccba693c4a
AndreaGuarracino <andreaguarracino@outlook.com>
2022-04-23 15:28:18 +0200

    update doc info [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -20,13 +20,13 @@ sys.path.insert(0, os.path.abspath('../lib/'))
 # -- Project information -----------------------------------------------------
 
 project = u'odgi'
-copyright = '2021, Guarracino A., Heumos S., Nahnsen S., Prins P., Garrison E. Revision v0.7.1-baedc51'
+copyright = '2020-2022, Guarracino A., Heumos S., Nahnsen S., Prins P., Garrison E. Revision v0.7.1-004f72f'
 author = u'Andrea Guarracino, Simon Heumos, Sven Nahnsen , Pjotr Prins, Erik Garrison'
 
 # The short X.Y version
 version = 'v0.7.1'
 # The full version, including alpha/beta/rc tags
-release = 'baedc51'
+release = '004f72f'
 
 
 # -- General configuration ---------------------------------------------------
