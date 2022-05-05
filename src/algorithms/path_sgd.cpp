8bd0cbc75686f759028aa7c6fd7772a46610686a
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2022-05-05 17:17:23 +0200

    we also need to update the progress meter

--- src/algorithms/path_sgd.cpp
+++ src/algorithms/path_sgd.cpp
@@ -303,6 +303,9 @@ namespace odgi {
 									if (!update_term_j && !update_term_i) {
 										// we also have to update the number of terms here, because else we will over sample and the sorting will take much longer
 										term_updates++; // atomic
+										if (progress) {
+											progress_meter->increment(1);
+										}
 										continue;
 									}
 
