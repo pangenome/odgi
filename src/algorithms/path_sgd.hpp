e00aefb975c81afd354224b2b2399e239b34f673
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2022-05-19 11:25:12 +0200

    try a fix

--- src/algorithms/path_sgd.hpp
+++ src/algorithms/path_sgd.hpp
@@ -21,11 +21,8 @@
 #include "dirty_zipfian_int_distribution.h"
 #include "XoshiroCpp.hpp"
 #include "progress.hpp"
-<<<<<<< HEAD
 #include "algorithms/layout.hpp"
-=======
 #include "utils.hpp"
->>>>>>> master
 
 #include <fstream>
 
@@ -87,7 +84,7 @@ std::vector<handle_t> path_linear_sgd_order(const graph_t &graph,
                                             const bool &snapshot,
                                             const std::string &snapshot_prefix,
                                             const bool &write_layout,
-                                            const std::string &layout_out);
+                                            const std::string &layout_out,
 											const bool &target_sorting,
 											std::vector<bool>& target_nodes);
 
