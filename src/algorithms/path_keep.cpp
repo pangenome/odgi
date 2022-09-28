46114a296f98befec7abcdac5e4f4078f32f2e55
Erik Garrison <erik.garrison@gmail.com>
2022-09-28 16:49:54 +0200

    also pass through the edges

--- src/algorithms/path_keep.cpp
+++ src/algorithms/path_keep.cpp
@@ -8,6 +8,17 @@ void keep_paths(const graph_t& graph, graph_t& into, const ska::flat_hash_set<pa
     graph.for_each_handle([&](const handle_t& h) {
         into.create_handle(graph.get_sequence(h), graph.get_id(h));
     });
+    graph.for_each_handle([&](const handle_t& h) {
+        graph.follow_edges(h, false, [&](const handle_t& t) {
+            into.create_edge(into.get_handle(graph.get_id(h), graph.get_is_reverse(h)),
+                             into.get_handle(graph.get_id(t), graph.get_is_reverse(t)));
+        });
+        auto r = graph.flip(h);
+        graph.follow_edges(r, false, [&](const handle_t& t) {
+            into.create_edge(into.get_handle(graph.get_id(r), graph.get_is_reverse(r)),
+                             into.get_handle(graph.get_id(t), graph.get_is_reverse(t)));
+        });
+    });
     std::vector<path_handle_t> paths;
     graph.for_each_path_handle([&](const path_handle_t& p) { paths.push_back(p); });
     // ensure we have the same path handle order as in the original graph
