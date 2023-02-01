eb5cafb9943607deff39e0474245eb346ef8f785
tonyjie <jiajieli0429@gmail.com>
2023-02-01 01:35:35 -0500

    [odgi tension] Set path_layout_dist and path_nuc_dist to 0 for every step. Otherwise it will accumulate among the path, which will lead to little difference of tension for the later steps in the path

--- src/subcommand/tension_main.cpp
+++ src/subcommand/tension_main.cpp
@@ -124,9 +124,11 @@ int main_tension(int argc, char **argv) {
 			std::string path_name = graph.get_path_name(p);
 			uint64_t cur_window_start = 1;
 			uint64_t cur_window_end = 0;
-			double path_layout_dist = 0;
-			uint64_t path_nuc_dist = 0;
+			double path_layout_dist;
+			uint64_t path_nuc_dist;
 			graph.for_each_step_in_path(p, [&](const step_handle_t &s) {
+				path_layout_dist = 0;
+				path_nuc_dist = 0;
 				handle_t h = graph.get_handle_of_step(s);
 				algorithms::xy_d_t h_coords_start;
 				algorithms::xy_d_t h_coords_end;
@@ -254,9 +256,11 @@ int main_tension(int argc, char **argv) {
 		// std::cout << "TEST\tPANGENOME\tMODE" << std::endl;
 #pragma omp parallel for schedule(static, 1) num_threads(thread_count) shared(node_tensions)
 		for (auto p: paths) {
-			double path_layout_dist = 0;
-			uint64_t path_nuc_dist = 0;
+			double path_layout_dist;
+			uint64_t path_nuc_dist;
 			graph.for_each_step_in_path(p, [&](const step_handle_t &s) {
+				path_layout_dist = 0;
+				path_nuc_dist = 0;
 				handle_t h = graph.get_handle_of_step(s);
 				algorithms::xy_d_t h_coords_start;
 				algorithms::xy_d_t h_coords_end;
